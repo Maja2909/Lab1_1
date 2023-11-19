5d45f433bfb05e38d963e770ab865ca83a366722
Maja2909 <m.klarzynska@onet.pl>
2023-11-19 11:36:27 +0100

    Zadanie 5

---
+++ src/CreateGUI.java
@@ -0,0 +1,13 @@
+//Zadanie 5 Magdalena Klarzyńska
+//Utworzenie nowego projektu Lab1_2a
+import javax.swing.JFrame;
+//Stworzenie klasy o nazwie CreateGUI
+public class CreateGUI{
+
+    public static void main(String[] args)
+    {
+        JFrame frame = new JFrame();
+        frame.setSize(640, 480); //Rozmiar okna
+        frame.setVisible(true);
+    }
+}
